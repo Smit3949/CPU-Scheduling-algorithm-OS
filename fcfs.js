a1933746c92038a310b2acbcae2651838614ff50
smit3949 <65903434+smit3949@users.noreply.github.com>
2020-12-01 11:29:08 +0530

    Update fcfs.js

--- fcfs.js
+++ fcfs.js
@@ -11,34 +11,23 @@ tuple.sort(function(a, b) {
   return a.art - b.art;
 });
 tuple.sort();
-
 var n = tuple.length;
-var wt = [];
-var tat = [];
-var total_wt = 0;
-var total_tat = 0;
-var rt = [];
-
-for(var i=0;i<n;i++){
-	rt.push(tuple[i].bt);
-}
-var complete=0, t=0,minm=9999;
-var shortest=0,finish_time;
-var check = false;
-var final_ans = [];
-var visited = [];
+var wt = [];// waiting time
+var tat = [];//turn around time
+var total_wt = 0;//total waiting time
+var total_tat = 0;//total turnaround time
+var final_ans = [];// grannt chart
+var visited = []; 
 for(var i=0;i<tuple.length;i++){
 	visited[i]=0;
 }
-var que = [];
+var que = []; // running queue 
 var state = 0;
 var flag=0;
 var count = 0;
-for(var i=0;i<50;i++){
+for(var i=0;i<10000;i++){
 	visited[state]++;
-
 	if(tuple[state].art>i){
-		
 		final_ans.push('/');
 		var smit =[];
 		que.push(smit);
@@ -53,26 +42,19 @@ for(var i=0;i<50;i++){
 			{	flag=1;
 				for(var j=0;j<tuple[state].bt;j++){
 					final_ans.push(tuple[state].pid);
-					
 				}
 				var mx=10000;
 				var ind=0;
-			
 				for(var j=0;j<tuple.length;j++){
 					if(tuple[j].art<=tuple[state].art+tuple[state].bt && visited[j]===0){
-						
 						if(tuple[j].art<mx){
-						
 							mx=tuple[j].art;
 							ind=j;
 						}
 					}
-				
 				}
-			
-			//	console.log(state + " " + visited[state] + " " + tuple[state].bt + " " );
 				for(var k=i;k<i+tuple[state].bt;k++){
-				 var smit = [];
+				 	var smit = [];
 					count++;
 					for(var y=0;y<n;y++){
 						if(tuple[y].art <= k && visited[y]<=1){
@@ -89,32 +71,20 @@ for(var i=0;i<50;i++){
 		
 	}
 }
-console.log(que.length);
-for(var i=0;i<50;i++){
-	console.log(final_ans[i]);
-}
-var cmp_time = [];
+var cmp_time = [];//completion time
 for(var i=0;i<tuple.length;i++){
-
 	cmp_time[i]=-1;
 }
 for(var i=final_ans.length-1;i>=0;i--){
-//	console.log(final_ans[i]);
 	if(final_ans[i]==='/'){}
 		else{
 		if(cmp_time[final_ans[i]-1]==-1){
 			
 			cmp_time[final_ans[i]-1]=i+1;
 		}}
-	
 }
-
-
-var wt  = [];
-
 for(var i=0;i<n;i++){
 	tat[i]=cmp_time[i] - tuple_temp[i].art;
-
 	wt[i] = tat[i]-tuple[i].bt;
 }
 for(var i=0;i<n;i++){
