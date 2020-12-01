41a2b64d285021a373687c0ed041b89fec8ef589
smit3949 <65903434+smit3949@users.noreply.github.com>
2020-12-01 16:31:11 +0530

    Update ljf.js

--- ljf.js
+++ ljf.js
@@ -8,43 +8,29 @@ var total_btt = [];
 var artt = [];
 for(var i=0;i<n;i++){
 	total_btt[i]=tuple[i].bt;
-}
-for(var i=0;i<n;i++)
-{	console.log(tuple[i].art);
 	artt[i]=tuple[i].art;
 }
-
-
 var tuple_temp = tuple;
 tuple.sort(function(a, b) {
   return a.art - b.art;
 });
 tuple.sort();
-
 var n = tuple.length;
 var wt = [];
 var tat = [];
 var total_wt = 0;
 var total_tat = 0;
-var rt = [];
-for(var i=0;i<n;i++){
-	rt.push(tuple[i].bt);
-}
-
-
 var final_ans = [];
 var visited = [];
 for(var i=0;i<tuple.length;i++){
 	visited[i]=0;
 }
-
 var que = [];
 var btco = [];
 for(var i=0;i<n;i++){
 	btco[i] = 0;
 }
-
-for(var i=0;i<50;i++){
+for(var i=0;i<10000;i++){
 	
 	for(var j=0;j<n;j++){
 		if(tuple[j].bt<=0){
@@ -62,21 +48,15 @@ for(var i=0;i<50;i++){
 			}
 		}
 	}
-//	console.log("i "+i);
-
-//	console.log(i+" sasd " +state);
 	if(state == -1){
 		final_ans.push('/');
 		var smit = [];
 		que.push(smit);
 	}
 	else{
-	//	console.log("a");
 			for(var j=0;j<tuple[state].bt;j++){
 				final_ans.push(tuple[state].pid);
 			}
-			
-		
 			for(var g=i;g<i+tuple[state].bt;g++){
 					var smit = [];
 					for(var y=0;y<n;y++){
@@ -86,11 +66,8 @@ for(var i=0;i<50;i++){
 					}
 					que.push(smit);
 			}
-			
 			i+=tuple[state].bt-1;
 			tuple[state].bt=0;
-			
-	
 	}
 
 }
@@ -104,25 +81,16 @@ for(var i=0;i<tuple.length;i++){
 	cmp_time[i]=-1;
 }
 for(var i=final_ans.length-1;i>=0;i--){
-//	console.log(final_ans[i]);
 	if(final_ans[i]==='/'){}
 		else{
 		if(cmp_time[final_ans[i]-1]==-1){
 			
 			cmp_time[final_ans[i]-1]=i+1;
 		}}
-	
 }
-
-
-var wt  = [];
-
 for(var i=0;i<n;i++){
-	console.log(cmp_time[i]+" "+artt[i]);
 	tat[i]=cmp_time[i] - artt[i];
-
 	wt[i] = tat[i]-total_btt[i];
-	
 }
 for(var i=0;i<n;i++){
 	total_wt = total_wt + wt[i];
